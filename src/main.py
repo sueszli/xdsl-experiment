c86c12d290cf2c55a93d0ba60e1564b273e426e1
sueszli <jabaryyahya@gmail.com>
2025-12-20 17:51:55 +0100

    up

--- src/main.py
+++ src/main.py
@@ -9,6 +9,9 @@ import argparse
 from pathlib import Path
 
 from xdsl.interpreter import Interpreter
+from xdsl.context import Context
+from xdsl.dialects import affine, arith, func, memref, printf, riscv, riscv_func, scf
+from xdsl.dialects.builtin import Builtin
 
 from frontend.ast_nodes import dump
 from frontend.ir_gen import IRGen
@@ -23,10 +26,8 @@ group.add_argument("--ast", action="store_true", help="print IR")
 group.add_argument("--mlir", action="store_true", help="print MLIR")
 parser.add_argument("--interpret", action="store_true", help="Interpret the code")
 args = parser.parse_args()
-
-filename = args.file
-assert filename.endswith(".aziz")
-src = Path(filename).read_text()
+assert args.file.endswith(".aziz")
+src = Path(args.file).read_text()
 
 module_ast = AzizParser("in_memory", src).parse_module()  # source -> ast
 if args.ast:
@@ -35,6 +36,27 @@ if args.ast:
 module_op = IRGen().ir_gen_module(module_ast)  # ast -> mlir
 if args.mlir:
     print(module_op, "\n")
+
+def context() -> Context:
+    ctx = Context()
+    ctx.load_dialect(affine.Affine)
+    ctx.load_dialect(arith.Arith)
+    ctx.load_dialect(Builtin)
+    ctx.load_dialect(func.Func)
+    ctx.load_dialect(memref.MemRef)
+    ctx.load_dialect(printf.Printf)
+    ctx.load_dialect(riscv_func.RISCV_Func)
+    ctx.load_dialect(riscv.RISCV)
+    ctx.load_dialect(scf.Scf)
+    # ctx.load_dialect(toy.Toy)
+    return ctx
+
+ctx = context()
+
+
+# code = compile(program)
+# emulate_riscv(code)
+
 
 if args.interpret:
     interpreter = Interpreter(module_op)
